0c8a1f3fde645a2264f8f873c7e186704527478e
Tung HOANG <tung.hoang@gmail.com>
2020-11-19 22:12:29 +0700

    add session and views

--- bookstore/src/main.js
+++ bookstore/src/main.js
@@ -2,14 +2,38 @@ const express = require('express');
 const booksRouter = require('./routes/books');
 const authorsRouter = require('./routes/authors');
 const app = express();
+const path = require('path');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const cors = require('cors');
 
 const {init} = require('./db');
 
+console.log(__dirname);
+
+app.use(express.static(__dirname + '/public'));
+app.use(cors());
 app.use(express.json());
+
+app.use(session({
+  secret: "bi mat",
+  store: new FileStore({}),
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60000
+  }
+}));
 
 app.use('/books', booksRouter);
 app.use('/authors', authorsRouter);
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+const viewPath = path.join(__dirname, 'views');
+console.log(viewPath);
+app.set('views', viewPath);
+console.log('set views already', viewPath);
 init().then(function() {
   app.listen(8000, function() {
     console.log("App started at port 8000");
--- bookstore/src/routes/books.js
+++ bookstore/src/routes/books.js
@@ -20,6 +20,36 @@ router.post('/list', async function(req, res) {
   });
 });
 
+router.get('/list', function(req, res) {
+  console.log(req.session.view);
+  if (req.session.view) {
+    console.log("inc by 1");
+    req.session.view = req.session.view + 1;
+  }
+  else {
+    console.log("set to 1");
+    req.session.view = 1;
+  }
+  Book.findAll().then(books => {
+    res.render('books', {books: books, view: req.session.view});
+  }).catch(e => {
+    res.send({
+      success: false,
+      data: e.message
+    });
+  });
+});
+
+router.get('/list/:idBook', function(req, res) {
+  let idBook = req.params.idBook;
+  Book.findByPk(idBook, {include: [Author]}).then(book => {
+    res.render('bookDetails', {book: book});
+  }).catch(e => res.send({
+    success: false,
+    data: e.message
+  }));
+});
+
 router.post('/new', function(req, res) {
   let book = Book.build(req.body);
   book.save().then(b => {
